073f25f10bff47cf578c3323f2e3c713a51cf096
Erin <erincconrad@gmail.com>
2021-04-26 13:08:07 -0400

    script to call new matlab instances

---
+++ for_borel/call_matlab_spike_detector.py
@@ -0,0 +1,18 @@
+import subprocess
+import sys
+
+def run_matlab(whichPts):
+
+    if len(whichPts) != 2:
+        print("Need two patients exactly")
+        raise
+
+    txt = '"matlab","-nodisplay","-nodesktop","-r",
+                             "addpath(genpath(''/mnt/local/gdrive/public/USERS/erinconr/projects/interictal_hubs/tools/''));'...
+    'cd ../get_hubs;dbstop if error;get_spikes([{} {}]);exit"'.format(whichPts[0],whichPts[1])
+    
+    subprocess.call([txt])
+
+if __name__ == '__main__':
+    whichPts = sys.argv
+    run_matlab(whichPts)
