a1e26d8735959a06dcccf539cc0f8139774bd006
Erin <erincconrad@gmail.com>
2021-04-26 10:25:00 -0400

    script to call new matlab instances

--- for_borel/new_spike_collection.m
+++ for_borel/new_spike_collection.m
@@ -4,7 +4,7 @@ if length(whichPts) ~= 2, error('Enter two patients'); end
 
 str = sprintf(['screen;matlab -nodisplay -r '...
     '"addpath(genpath(''/mnt/local/gdrive/public/USERS/erinconr/projects/interictal_hubs/tools/''));'...
-    'cd ../get_hubs;get_spikes([%d %d]);exit"'],whichPts(1),whichPts(2));
+    'cd ../get_hubs;get_spikes([%d %d]);exit";exit'],whichPts(1),whichPts(2));
 
 system(str);
 
