a202da2f1dbca69aeae6a988f66763b6ba484c07
Erin <erincconrad@gmail.com>
2021-04-27 19:31:53 -0400

    do not remember

--- eeg_prep/pull_detector_params.m
+++ eeg_prep/pull_detector_params.m
@@ -3,14 +3,15 @@ function params = pull_detector_params(name,T)
 
 %% Get the relevant row
 row = find(strcmp(T.Patient,name));
+
 tmul = T.Tmul(row);
 absthresh = T.Absthresh(row);
 
 %% Default values if not specified
-if isnan(tmul)
+if isnan(tmul) || isempty(row)
     tmul = 17;
 end
-if isnan(absthresh)
+if isnan(absthresh) || isempty(row)
     absthresh = 50;
 end
 
