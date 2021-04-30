e7793c37b04007befa0dcb644c6442827bc40466
Erin <erincconrad@gmail.com>
2021-04-30 14:18:56 -0400

    switched bipolar to car

--- eeg_prep/new_pre_process.m
+++ eeg_prep/new_pre_process.m
@@ -1,6 +1,6 @@
 function values = new_pre_process(values,which_chs)
 
 % Just do a car (just average the non-skip chs)
-values = values - repmat(mean(values(:,which_chs),2),1,size(values,2));
+values = values - repmat(nanmean(values(:,which_chs),2),1,size(values,2));
 
 end
